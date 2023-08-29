1a98ecb058433ec6be57cb4ee24b3c3eef6e9ef9
DigitalBogorm <122612269+DigitalBogorm@users.noreply.github.com>
2023-08-29 13:55:26 +0200

    V1.1
      -Patch til password, nu bør man kunne sætte andre end "1234"
    
      -Tilføjet pseudo-interaktion mellem forskellige komponenter (dvs. de giver nu information, som brugeren kan få nytte af).
    
      -Tilføjet farveskifte ved LCD-skærm. Udelukkende visuelt,

--- PuzzleBox.ino
+++ PuzzleBox.ino
@@ -2,10 +2,10 @@
 #define RotSens1 A0
 #define RotSens2 A1
 //Ved ikke hvad det gør, men koden kan ikke lide, at man fjerner det.
-#define ADC_REF 5       //reference voltage of ADC is 5v.If the Vcc switch on the seeeduino \
+#define ADC_REF 5      //reference voltage of ADC is 5v.If the Vcc switch on the seeeduino \
                         //board switches to 3V3, the ADC_REF should be 3.3
-#define GROVE_VCC 5     //VCC of the grove interface is normally 5v
-#define FULL_ANGLE 300  //full value of the rotary angle is 300 degrees
+#define GROVE_VCC 5    //VCC of the grove interface is normally 5v
+#define FULL_ANGLE 99  //full value of the rotary angle is 300 degrees
 //Her opsættes 7-segment-display
 #include "TM1637.h"
 //Porte til 4-digit-display defineres her. juster efter behov.
@@ -16,6 +16,8 @@ TM1637 tm1637(CLK, DIO);
 int num;
 //Ved ikke hvad det her er, men det virker vigtigt.
 #include "SoftwareSerial.h"
+
+#define LED 4
 
 //Et eller andet sted i det her rod, bliver keypad'en defineret som port D2, tror jeg nok.
 #if defined(ARDUINO_ARCH_AVR)
@@ -48,7 +50,7 @@ bool read = true;
 const int passLength = 4;
 int currentLength = 0;
 //Øverste er det aktuelle password, nederste er brugerens input
-int password[passLength] = { 1, 2, 3, 4 };
+int password[passLength] = {6, 8, 4, 4};
 int inputKeypad[passLength];
 
 
@@ -56,6 +58,9 @@ int inputKeypad[passLength];
 void setup() {
   //Kører "serial", som er der keypad'en outputter til.
   TRANS_SERIAL.begin(9600);
+
+  //Fastsætter LED'en
+  pinMode(LED, OUTPUT);
 
   SERIAL.begin(9600);  // start serial for output
   SERIAL.println("Version:v1.0");
@@ -69,16 +74,21 @@ void setup() {
 
   lcd.setRGB(colorR, colorG, colorB);
 
+  digitalWrite(LED, HIGH);
+
   //Rotations-sensorerne er sat til at være input
   pinMode(RotSens1, INPUT);
   pinMode(RotSens2, INPUT);
   //Tænder for 7-segment-display
   tm1637.init();
   tm1637.set(BRIGHT_TYPICAL);
+  lcd.print("Online");
+  delay(2000);
+  lcd.clear();
 }
 
 void loop() {
-  //Ved ikke, don't touch it
+  //Kører den funktion, der får keypad'en til at virke. Do not delete
   printData();
   //Måler vinklen på den ene rotations-sensor (Den i A0)
   float voltage1;
@@ -91,9 +101,18 @@ void loop() {
   voltage2 = (float)sensor_value2 * ADC_REF / 1023;
   float degrees2 = (voltage2 * FULL_ANGLE) / GROVE_VCC;
   //Num danner et tal, baseret på inputtet fra rotations-sensorer
-  num = floor(degrees1 / 3.3) * 100 + floor(degrees2 / 3.3);
+  num = round(degrees1) * 100 + round(degrees2);
   //4-digit-display viser et tal, baseret på num (+10000-delen, er for at den viser '0000', hvis alt er på 0)
   tm1637.displayNum(10000 + num);
+  if (num == 2456) {
+    //SERIAL.println("TestSuccess");
+    //digitalWrite(LED, HIGH);
+    tone(buzzer, 1000);
+  } else {
+    //SERIAL.println("TestFailure");
+    //digitalWrite(LED, LOW);
+    noTone(buzzer);
+  }
 }
 
 //Denne funktion sørger for at læse keypad'ets input, og tjekker om koden stemmer overens
@@ -104,9 +123,9 @@ void assign(int value) {
   //Det her ændrer værdien for et punkt i inputpasswordet
   inputKeypad[currentLength] = value;
   //buzzerens lyd fungerer her vha, "tone", fordi alternativet er en helvedes støj, der driver alle i lokalet til vanvid.
-  tone(buzzer, 1000);
+  /*tone(buzzer, 1000);
   delay(100);
-  noTone(buzzer);
+  noTone(buzzer);*/
   lcd.print(value);
   currentLength++;
 
@@ -115,25 +134,29 @@ void assign(int value) {
     read = false;
     delay(1000);
     bool correct = true;
-    for (int i : password) {
+    for (int i = 0; i < passLength; i++) {
       //hvis ikke et ciffer af inputtet stemmer overens med det tilsvarende ciffer i passwordet, aktiveres fejlbeskeden.
-      if (inputKeypad[i - 1] != password[i - 1]) {
+      if (inputKeypad[i] != password[i]) {
         SERIAL.println("incorrect");
         lcd.clear();
+        colouring(255, 0, colorB);
         lcd.print("incorrect");
         correct = false;
         delay(1000);
+        colouring(colorR, colorG, colorB);
         lcd.clear();
         break;
       }
     }
     //hvis ikke der er problemer, aktiveres nedenstående. Indtil videre, er det bare en placeholder-besked.
     if (correct == true) {
-      delay(500);
+      delay(250);
       lcd.clear();
       SERIAL.println("confirmed");
-      lcd.print("confirmed");
-      delay(1000);
+      colouring(colorR, 255, colorB);
+      lcd.print("2456");
+      delay(3000);
+      colouring(colorR, colorG, colorB);
       lcd.clear();
     }
     currentLength = 0;
@@ -144,7 +167,9 @@ void assign(int value) {
 }
 //Læser inputtet fra keypad
 void printData() {
+
   uint8_t data = TRANS_SERIAL.read();
+  //SERIAL.println(data);
   switch (data) {
     case 0xE1:
       SERIAL.println("1");
@@ -187,11 +212,17 @@ void printData() {
       break;
     case 0xEB:
       SERIAL.println("0");
+      assign(0);
       break;
     case 0xEC:
       SERIAL.println("#");
       break;
     default:
+      //SERIAL.println("L + ratio");
       break;
   }
+}
+
+void colouring(int R, int G, int B) {
+  lcd.setRGB(R, G, B);
 }
